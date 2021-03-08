849360800de9eed1685479d310c8e65ea7c0a197
AnastasiyaPilushina <pilushina22@gmail.com>
2021-03-08 12:35:38 +0300

    Added manager

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -1,14 +1,22 @@
 package ru.netology.manager;
 
-import ru.netology.domain.PurchaseItem;
+import ru.netology.domain.Movie;
 
-public class CartManager {
-    private PurchaseItem[] items = new PurchaseItem[0];
 
-    public void add(PurchaseItem item) {
+public class MovieManager {
+    private Movie[] items = new Movie[0];
+
+    public MovieManager(int afishalenght) {
+        this.afishalenght = afishalenght;
+    }
+    public MovieManager() {}
+
+    private int afishalenght = 7;
+
+    public void addMovie(Movie item) {
         // создаём новый массив размером на единицу больше
         int length = items.length + 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
+        Movie[] tmp = new Movie[length];
         // itar + tab
         // копируем поэлементно
         // for (int i = 0; i < items.length; i++) {
@@ -21,8 +29,8 @@ public class CartManager {
         items = tmp;
     }
 
-    public PurchaseItem[] getAll() {
-        PurchaseItem[] result = new PurchaseItem[items.length];
+    public Movie[] getAll() {
+        Movie[] result = new Movie[items.length];
         // перебираем массив в прямом порядке
         // но кладём в результаты в обратном
         for (int i = 0; i < result.length; i++) {
@@ -35,9 +43,9 @@ public class CartManager {
     // наивная реализация
     public void removeById(int id) {
         int length = items.length - 1;
-        PurchaseItem[] tmp = new PurchaseItem[length];
+        Movie[] tmp = new Movie[length];
         int index = 0;
-        for (PurchaseItem item : items) {
+        for (Movie item : items) {
             if (item.getId() != id) {
                 tmp[index] = item;
                 index++;
